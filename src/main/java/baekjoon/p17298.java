1abbdee4d18ea97ea7778a83395528a1f2128110
wooqqq <sge0225@naver.com>
2024-02-12 16:39:55 +0900

    [17298] 오큰수

---
+++ src/main/java/baekjoon/p17298.java
@@ -0,0 +1,41 @@
+package baekjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Stack;
+import java.util.StringTokenizer;
+
+public class p17298 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+        int N = Integer.parseInt(br.readLine());
+        int[] arr = new int[N];
+        Stack<Integer> stack = new Stack<>();
+
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        for (int i = 0; i < N; i++) {
+            while (!stack.isEmpty() && arr[stack.peek()] < arr[i]) {
+                arr[stack.pop()] = arr[i];
+            }
+            stack.push(i);
+        }
+
+        while (!stack.isEmpty()) {
+            arr[stack.pop()] = -1;
+        }
+
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < N; i++) {
+            sb.append(arr[i]).append(" ");
+        }
+        System.out.println(sb);
+    }
+}
