ab21d50989e286d444b65665fb55d5d072314d40
wooqqq <sge0225@naver.com>
2024-03-01 21:28:29 +0900

    [10157] 자리배청

---
+++ src/main/java/baekjoon/p10157.java
@@ -0,0 +1,30 @@
+package baekjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class p10157 {
+
+    static int C, R, K;
+    static int[][] arr;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        StringBuilder sb = new StringBuilder();
+
+        st = new StringTokenizer(br.readLine());
+        C = Integer.parseInt(st.nextToken());
+        R = Integer.parseInt(st.nextToken());
+        arr = new int[R][C];
+
+        K = Integer.parseInt(br.readLine());
+
+        if (K > C * R)
+            sb.append("0");
+        else
+
+    }
+}
