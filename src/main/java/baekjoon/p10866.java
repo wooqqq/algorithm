840790b6463bcd858f2f9d0bc0cc123775061ee7
wooqqq <sge0225@naver.com>
2024-03-15 23:56:04 +0900

    [10866] 덱

---
+++ src/main/java/baekjoon/p10866.java
@@ -0,0 +1,87 @@
+package baekjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayDeque;
+
+public class p10866 {
+
+    static int N;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        N = Integer.parseInt(br.readLine());
+        ArrayDeque<Integer> dq = new ArrayDeque<>();
+
+        for (int i = 0; i < N; i++) {
+            String[] input = br.readLine().split(" ");
+
+            switch (input[0]) {
+                case "push_front": {
+                    dq.addFirst(Integer.parseInt(input[1]));
+                    break;
+                }
+
+                case "push_back": {
+                    dq.addLast(Integer.parseInt(input[1]));
+                    break;
+                }
+
+                case "pop_front": {
+                    if (dq.isEmpty()) {
+                        sb.append(-1).append('\n');
+                    } else {
+                        sb.append(dq.pollFirst()).append('\n');
+                    }
+                    break;
+                }
+
+                case "pop_back": {
+                    if (dq.isEmpty()) {
+                        sb.append(-1).append('\n');
+                    } else {
+                        sb.append(dq.pollLast()).append('\n');
+                    }
+                    break;
+                }
+
+                case "size": {
+                    sb.append(dq.size()).append('\n');
+                    break;
+                }
+
+                case "empty": {
+                    if (dq.isEmpty()) {
+                        sb.append(1).append('\n');
+                    } else {
+                        sb.append(0).append('\n');
+                    }
+                    break;
+                }
+
+                case "front": {
+                    if (dq.isEmpty()) {
+                        sb.append(-1).append('\n');
+                    } else {
+                        sb.append(dq.peekFirst()).append('\n');
+                    }
+                    break;
+                }
+
+                case "back": {
+                    if (dq.isEmpty()) {
+                        sb.append(-1).append('\n');
+                    } else {
+                        sb.append(dq.peekLast()).append('\n');
+                    }
+                    break;
+                }
+            }
+
+        }
+        System.out.println(sb);
+    }
+}
