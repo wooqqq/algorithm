49b174037a21a8c51e198731fd149fae1d288fe3
깃wooqqq <sge0225@naver.com>
2024-03-27 23:30:23 +0900

    [swea 7733] 치즈 도둑 - DFS

--- src/main/java/swea/p7733_DFS.java
+++ src/main/java/swea/p7733_DFS.java
@@ -4,23 +4,20 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Arrays;
-import java.util.LinkedList;
-import java.util.Queue;
 import java.util.StringTokenizer;
 
-public class p7733_BFS {
+public class p7733_DFS {
 
     static int N;
     static int[][] cheese;
     static boolean[][] visited;
     static int[] dr = {-1, 1, 0, 0}; // 상, 하, 좌, 우
     static int[] dc = {0, 0, -1, 1};
-    static Queue<int[]> queue;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringBuilder sb;
         StringTokenizer st;
+        StringBuilder sb;
 
         int T = Integer.parseInt(br.readLine());
 
@@ -33,7 +30,7 @@ public class p7733_BFS {
             cheese = new int[N][N];
             visited = new boolean[N][N];
 
-            // 치즈 각 칸의 맛있는 정도 입력받기
+            // 치즈 입력받기
             for (int r = 0; r < N; r++) {
                 st = new StringTokenizer(br.readLine());
                 for (int c = 0; c < N; c++) {
@@ -41,18 +38,17 @@ public class p7733_BFS {
                 }
             }
 
-            queue = new LinkedList<>();
-
             int cnt = 0;
             int max = Integer.MIN_VALUE;
+
             for (int i = 0; i <= 100; i++) {
                 cnt = 0;
-
                 resetVisit();
+                
                 for (int r = 0; r < N; r++) {
                     for (int c = 0; c < N; c++) {
                         if (cheese[r][c] > i && !visited[r][c]) {
-                            bfs(r, c, i);
+                            dfs(r, c, i);
                             cnt++;
                         }
                     }
@@ -67,31 +63,20 @@ public class p7733_BFS {
 
     }
 
-    private static void bfs(int r, int c, int day) {
-        queue.add(new int[] {r, c});
+    static void dfs(int r, int c, int day) {
         visited[r][c] = true;
 
-        while (!queue.isEmpty()) {
-            int[] input = queue.poll();
-            int x = input[0];
-            int y = input[1];
+        for (int d = 0; d < 4; d++) {
+            int nr = r + dr[d];
+            int nc = c + dc[d];
 
-            // 델타배열을 통한 사방탐색
-            for (int d = 0; d < 4; d++) {
-                int nr = x + dr[d];
-                int nc = y + dc[d];
+            if (nr < 0 || nr >= N || nc < 0 || nc >= N) continue;
 
-                if (nr < 0 || nr >= N || nc < 0 || nc >= N) continue;
-
-                if (cheese[nr][nc] > day && !visited[nr][nc]) {
-                    queue.add(new int[] {nr, nc});
-                    visited[nr][nc] = true;
-                }
-            }
+            if (cheese[nr][nc] > day && !visited[nr][nc]) dfs(nr, nc, day);
         }
     }
 
-    private static void resetVisit() {
+    static void resetVisit() {
         for (int i = 0; i < N; i++) {
             Arrays.fill(visited[i], false);
         }
